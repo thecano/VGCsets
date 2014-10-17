75bd1f033c6df8bf5b6166f2fbab0402c9ae3e37
Alejandro Díaz <ajdiaz@alumnos.inf.utfsm.cl>
2014-10-16 21:10:46 -0300

    bootstrap basico

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141016230318) do
+ActiveRecord::Schema.define(version: 20141016233555) do
 
   create_table "pokemonsets", force: true do |t|
     t.string   "specie"
