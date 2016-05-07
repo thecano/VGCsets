48a2b18658f057587200e61c197a0e4dab049e24
Alejandro Díaz <ajdiaz@alumnos.inf.utfsm.cl>
2016-05-07 17:18:08 -0300

    intento de arreglar el redirect con subdominios 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,9 +22,12 @@ end
 
 def ensure_subdomain
     if request.subdomain==""
-      redirect_to ("http://"+I18n.locale.to_s+"."+"vgcsets.com"+request.path)
+	logger.debug "entra el if vacio"      
+	redirect_to ("http://"+I18n.locale.to_s+"."+"vgcsets.com"+request.path)      
     elsif request.subdomain == "www"
+	logger.debug "entra el if www"	
 	redirect_to ("http://"+I18n.locale.to_s+"."+"vgcsets.com"+request.path)
+	
 	  #redirect_to request.url.sub("www","")
     #else 
     #  redirect_to "http://www.rubyonrails.org"
