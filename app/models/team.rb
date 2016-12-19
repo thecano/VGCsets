736daf4745c699db9dc6b8d58de328e0ec58d87f
Alejandro Díaz <ajdiaz@alumnos.inf.utfsm.cl>
2016-12-19 19:23:48 -0300

    se agrega orden de torneo por fecha en perfil jugador

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,25 @@
 class Team < ActiveRecord::Base
 belongs_to :player
 has_one :top
+attr_accessor :fecha #both
+	
+	def fecha
+	 if !Top.where(:team1_id=>self.id).empty?
+	 	self.fecha = Top.where(:team1_id=>self.id).first.fecha
+	 elsif !Top.where(:team2_id=>self.id).empty?
+	 	self.fecha = Top.where(:team2_id=>self.id).first.fecha
+	 elsif !Top.where(:team3_id=>self.id).empty?
+	 	self.fecha = Top.where(:team3_id=>self.id).first.fecha
+	 elsif !Top.where(:team4_id=>self.id).empty?
+	 	self.fecha = Top.where(:team4_id=>self.id).first.fecha
+	 elsif !Top.where(:team5_id=>self.id).empty?
+	 	self.fecha = Top.where(:team5_id=>self.id).first.fecha
+	 elsif !Top.where(:team6_id=>self.id).empty?
+	 	self.fecha = Top.where(:team6_id=>self.id).first.fecha
+	 elsif !Top.where(:team7_id=>self.id).empty?
+	 	self.fecha = Top.where(:team7_id=>self.id).first.fecha
+	 elsif !Top.where(:team8_id=>self.id).empty?
+	 	self.fecha = Top.where(:team8_id=>self.id).first.fecha
+	 end
+	end
 end
