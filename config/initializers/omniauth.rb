8c7544878350ec0945e2351880290d6312fb2098
Alejandro Díaz <cano.diaz@gmail.com>
2017-03-29 21:33:59 -0300

    a

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
- 		provider :facebook, Rails.application.secrets.facebook_key, Rails.application.secrets.facebook_secret
+ 		provider :facebook, Rails.application.secrets.facebook_key, Rails.application.secrets.facebook_secret,
+    client_options: {
+      site: 'https://graph.facebook.com/v2.6',
+      authorize_url: "https://www.facebook.com/v2.6/dialog/oauth"
+    }
  		provider :twitter, Rails.application.secrets.twitter_key, Rails.application.secrets.twitter_secret
       
 end
