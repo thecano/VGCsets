28b2cd9be9c496a41004b87f2023c7b8d2f26700
Alejandro Díaz <ajdiaz@alumnos.inf.utfsm.cl>
2018-01-16 17:30:44 -0300

    se agrega relacion a modelos para author

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,5 @@ class Player < ActiveRecord::Base
   def to_param
     [id, nickname.parameterize].join("-")
   end
+  belongs_to :user, :foreign_key = "author"
 end
--- app/models/top.rb
+++ app/models/top.rb
@@ -1,4 +1,4 @@
 class Top < ActiveRecord::Base
-
+belongs_to :user, :foreign_key = "author"
 
 end
