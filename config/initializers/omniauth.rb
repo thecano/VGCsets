6efe65f03fb6759e81039ad29676743288fa427b
Alejandro Díaz <cano.diaz@gmail.com>
2019-01-14 00:22:37 -0300

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,9 +3,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     client_options: {
       site: 'https://graph.facebook.com/v2.9',
       authorize_url: "https://www.facebook.com/v2.9/dialog/oauth"
-    },
-    token_params: {
-      parse: :json
     }
  		provider :twitter, ENV['twitter_key'], ENV['twitter_secret']
       
