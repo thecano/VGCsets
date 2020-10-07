1ecdf18513e1e6aeef91dd56fea38a3e616576c9
Alejandro Díaz <cano.diaz@gmail.com>
2020-10-07 00:39:37 -0300

    Update teams_controller.rb

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -41,7 +41,6 @@ class TeamsController < ApplicationController
       format.html
       format.png do
         kit = IMGKit.new render_to_string, width: 810
-        kit.stylesheets << Rails.root.join('public','/assets/application-d148f484e77abd44a91affcae5fccfb7e31c2b49f70f7aceb2ca07ac80c07636.css').to_s
         kit.javascripts << Rails.root.join('app','assets/javascripts/application.js').to_s
         send_data kit.to_png, type: "image/png", disposition: "default", layout: "screenshot"
       end
